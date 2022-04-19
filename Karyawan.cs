b4a393d48d527597cbb95eda3ca1d04bcd03fe7a
fauzan <fauzanlutfimuzaki24@gmail.com>
2022-04-19 16:23:10 +0700

    Inisialisasi Project

---
+++ Karyawan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4309
+{
+    class Karyawan
+    {
+        public string NIK { get; set; }
+        public string Nama{ get; set; }
+        public float GajiBulanan { get; set; }
+
+        public Karyawan(string nik, string nama, float gajibulanan)
+        {
+            NIK = nik;
+            Nama = nama;
+            GajiBulanan = gajibulanan;
+
+            if (GajiBulanan < 0)
+            {
+                GajiBulanan = 0;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4309
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan Paijo = new Karyawan("190302123", "Paijo", 3000000);
+            Karyawan Jono = new Karyawan("190302124", "Jono", 2000000);
+
+            Console.WriteLine("No Nik/Nama            Gaji Bulanan ");
+            Console.WriteLine("------------------------------------");
+            Console.Write("1. {0}", Paijo.NIK);
+            Console.Write(" {0}",Paijo.Nama);
+            Console.WriteLine("     {0}", Paijo.GajiBulanan);
+            Console.Write("1. {0}", Jono.NIK);
+            Console.Write(" {0}", Jono.Nama);
+            Console.WriteLine("      {0}", Jono.GajiBulanan);
+            Console.WriteLine();
+            Console.WriteLine("Asyiiiik kenaikan gaji 10%");
+            Console.WriteLine();
+
+            float Paijo10 = Paijo.GajiBulanan + (Paijo.GajiBulanan / 100 * 10);
+            float Jono10 = Jono.GajiBulanan + (Jono.GajiBulanan / 100 * 10);
+            Console.WriteLine("No Nik/Nama            Gaji Bulanan ");
+            Console.WriteLine("------------------------------------");
+            Console.Write("1. {0}", Paijo.NIK);
+            Console.Write(" {0}", Paijo.Nama);
+            Console.WriteLine("     {0}", Paijo10);
+            Console.Write("1. {0}", Jono.NIK);
+            Console.Write(" {0}", Jono.Nama);
+            Console.WriteLine("      {0}", Jono10);
+            Console.ReadKey();
+        }
+    }
+}
